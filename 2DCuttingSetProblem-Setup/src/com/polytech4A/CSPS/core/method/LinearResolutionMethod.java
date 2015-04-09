c493abdf0a9dec26e769204358b91a88fe2e98d3
agaldeano <alexandre.galdeano@etu.univ-lyon1.fr>
2015-04-09 18:28:32 +0200

    [evol] Début d'implémentation de la résolution linéaire

--- 2DCuttingSetProblem-Setup/src/com/polytech4A/CSPS/core/method/LinearResolutionMethod.java
+++ 2DCuttingSetProblem-Setup/src/com/polytech4A/CSPS/core/method/LinearResolutionMethod.java
@@ -1,23 +1,25 @@
 package com.polytech4A.CSPS.core.method;
 
 import com.polytech4A.CSPS.core.model.Solution;
+import com.polytech4A.CSPS.core.util.Log;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.ArrayList;
 
 public class LinearResolutionMethod {
-	public static ArrayList<Integer> getCount(Solution solution) {
+	public ArrayList<Integer> getCount(Solution solution) {
 		// TODO
 		throw new NotImplementedException();
 	}
 
-	public static Long getFitness(Solution solution, Long costOfPattern,
+	public Long getFitness(Solution solution, Long costOfPattern,
 			Long costOfPrinting) {
 		ArrayList<Integer> count = getCount(solution);
 		Long prints = count
 				.parallelStream()
 				.mapToLong(i -> i.longValue()).sum();
-
-		return prints * costOfPrinting + count.size() * costOfPattern;
+		Long fitness = prints * costOfPrinting + count.size() * costOfPattern;
+		Log.log.trace(String.format("fitness = %s", fitness));
+		return fitness;
 	}
 }
