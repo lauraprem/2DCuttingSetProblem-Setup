5f0784e5cfac4cb3463cae2de37ce6b9789be991
agaldeano <alexandre.galdeano@etu.univ-lyon1.fr>
2015-04-09 18:55:51 +0200

    [evol] Ajout du context

---
+++ 2DCuttingSetProblem-Setup/src/com/polytech4A/CSPS/core/resolution/util/context/Context.java
@@ -0,0 +1,109 @@
+/*
+ *
+ *  * Project to resolve 2D cutting stock problem for Discreet Optimizations course at Polytech Lyon
+ *  * Copyright (C) 2015.  CARON Antoine and CHAUSSENDE Adrien
+ *  *
+ *  * This program is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU Affero General Public License as
+ *  * published by the Free Software Foundation, either version 3 of the
+ *  * License, or (at your option) any later version.
+ *  *
+ *  * This program is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  * GNU Affero General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU Affero General Public License
+ *  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.polytech4A.CSPS.core.resolution.util.context;
+
+import com.polytech4A.CSPS.core.model.Image;
+import com.polytech4A.CSPS.core.model.Vector;
+import com.sun.istack.internal.NotNull;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Antoine CARON on 12/03/2015.
+ *
+ * @author Antoine CARON
+ * @version 1.0
+ *          <p>
+ *          Context Objet for Serialization in XML file.
+ */
+public class Context {
+
+	/**
+	 * Scale of the project
+	 */
+	private Long scale;
+
+	/**
+	 * Id of the context
+	 */
+	private String label;
+
+	/**
+	 * Cost of a Pattern in .
+	 */
+	private int patternCost;
+
+	/**
+	 * Cost of a sheet in .
+	 */
+	private int sheetCost;
+
+	/**
+	 * Size of a Pattern.
+	 */
+	private Vector patternSize;
+
+	/**
+	 * Boxes with size and amount to print, not the amount per pattern.
+	 */
+	@NotNull private ArrayList<Image> images;
+
+	public Context(String label, int patternCost, int sheetCost, @NotNull ArrayList<Image> images, Vector patternSize) {
+		this(label, patternCost, sheetCost, images, patternSize, 1L);
+	}
+
+	public Context(String label, int patternCost, int sheetCost, @NotNull ArrayList<Image> images, Vector patternSize, Long scale) {
+		this.label = label;
+		this.patternCost = patternCost;
+		this.sheetCost = sheetCost;
+		this.images = images;
+		this.patternSize = patternSize;
+		this.scale = scale;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public int getPatternCost() {
+		return patternCost;
+	}
+
+	public int getSheetCost() {
+		return sheetCost;
+	}
+
+	public ArrayList<Image> getImages() {
+		return images;
+	}
+
+	public Vector getPatternSize() {
+		return patternSize;
+	}
+
+	public Long getScale() {
+		return scale;
+	}
+
+	public void setScale(Long scale) {
+		this.scale = scale;
+	}
+}
---
+++ 2DCuttingSetProblem-Setup/src/com/polytech4A/CSPS/core/resolution/util/context/ContextLoaderUtils.java
@@ -0,0 +1,89 @@
+package com.polytech4A.CSPS.core.resolution.util.context;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.polytech4A.CSPS.core.model.Image;
+import com.polytech4A.CSPS.core.model.Vector;
+import org.apache.commons.io.LineIterator;
+import org.apache.commons.io.FileUtils;
+
+/**
+ * Created by Antoine CARON on 20/03/2015.
+ *
+ * @author Antoine CARON
+ * @version 1.0
+ *          <p>
+ *          Loader of Context files.
+ */
+public class ContextLoaderUtils {
+	public static final Long SCALE = 1000L;
+	/**
+	 * Load the content of the context file.
+	 *
+	 * @param path path of the context File.
+	 * @return Context loaded.
+	 * @throws IOException                   if file not found.
+	 * @throws MalformedContextFileException if the Context file don't have the right structure.
+	 */
+	public static Context loadContext(String path) throws IOException {
+		File file = new File(path);
+		LineIterator it = FileUtils.lineIterator(file, "UTF-8");
+		ArrayList<Image> images = new ArrayList<>();
+		try {
+			Double x = loadLine(it, "^LX=[0-9]{1,13}(\\.[0-9]*)?$");
+			Double y = loadLine(it, "LY=[0-9]{1,13}(\\.[0-9]*)?");
+			int cost = loadLine(it, "m=[0-9]{1,13}(\\.[0-9]*)?").intValue();
+			while (it.hasNext()) {
+				images.add(loadImage(it.nextLine()));
+			}
+			LineIterator.closeQuietly(it);
+			return new Context(file.getName(), cost, 1, images, new Vector(Math.round(x*SCALE), Math.round(y*SCALE)));
+		} catch (MalformedContextFileException mctx) {
+			throw mctx;
+		} finally {
+			LineIterator.closeQuietly(it);
+		}
+	}
+
+	/**
+	 * Line Parser of global first infomrations.
+	 *
+	 * @param iterator iterator of the openfile.
+	 * @param regex    regex of the line.
+	 * @return value loaded.
+	 * @throws MalformedContextFileException if the Context file don't have the right structure.
+	 */
+	private static Double loadLine(LineIterator iterator, String regex) throws MalformedContextFileException {
+		MalformedContextFileException mctx = new MalformedContextFileException();
+		if (iterator.hasNext()) {
+			String line = iterator.nextLine();
+			if (line.matches(regex)) {
+				return Double.parseDouble(line.split("=")[1]);
+			} else {
+				throw mctx;
+			}
+		} else {
+			throw mctx;
+		}
+	}
+
+	/**
+	 * Load a box form the file.
+	 *
+	 * @param line line in the file.
+	 * @return Box loaded of the line
+	 * @throws MalformedContextFileException if the Context file don't have the right structure.
+	 */
+	private static Image loadImage(String line) throws MalformedContextFileException {
+		MalformedContextFileException mctx = new MalformedContextFileException();
+		if (line.matches("[0-9]{1,13}(\\.[0-9]*)?\\t[0-9]{1,13}(\\.[0-9]*)?\\t\\d{1,5}")) {
+			String[] array = line.split("\\t");
+			Long x = Math.round(Double.parseDouble(array[0]) * SCALE),
+					y = Math.round(Double.parseDouble(array[1]) * SCALE);
+			return new Image(new Vector(x, y), Long.parseLong(array[2]));
+		} else throw mctx;
+	}
+
+}
