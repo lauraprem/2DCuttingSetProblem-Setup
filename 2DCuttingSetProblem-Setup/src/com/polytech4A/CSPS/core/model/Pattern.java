7ab281f9172c37e93d642f024d8ccf67ed357fba
Laura <premillieu.laura@gmail.com>
2015-04-15 16:14:19 +0200

    merge

--- 2DCuttingSetProblem-Setup/src/com/polytech4A/CSPS/core/model/Pattern.java
+++ 2DCuttingSetProblem-Setup/src/com/polytech4A/CSPS/core/model/Pattern.java
@@ -1,8 +1,8 @@
 package com.polytech4A.CSPS.core.model;
 
-import java.util.ArrayList;
-
 import static com.polytech4A.CSPS.core.util.Util.escToString;
+
+import java.util.ArrayList;
 
 /**
  * Planche contenant des images
@@ -11,59 +11,65 @@ import static com.polytech4A.CSPS.core.util.Util.escToString;
  */
 public class Pattern {
 
-    /**
-     * taille du pattern
-     */
-    private Vector size;
+	/**
+	 * taille du pattern
+	 */
+	private Vector size;
 
-    /**
-     * Nombre de fois qu'il faut imprimer le pattern
-     */
-    private Long amount;
+	/**
+	 * Nombre de fois qu'il faut imprimer le pattern
+	 */
+	private Long amount;
 
-    /**
-     * Les images présente dans le pattern
-     */
-    private ArrayList<Image> listImg;
+	/**
+	 * Les images présente dans le pattern
+	 */
+	private ArrayList<Image> listImg;
 
-    public Pattern(Vector size, Long amount) {
-        super();
-        this.size = size;
-        this.amount = amount;
-    }
+	public Pattern(Vector size, Long amount) {
+		super();
+		this.size = size;
+		this.amount = amount;
+	}
 
-    public Pattern(Vector size, ArrayList<Image> listImg) {
-        this.size = size;
-        this.listImg = listImg;
-    }
+	public Pattern(Vector size, ArrayList<Image> listImg) {
+		this.size = size;
+		this.listImg = listImg;
+	}
 
-    public Vector getSize() {
-        return size;
-    }
+	public Vector getSize() {
+		return size;
+	}
 
-    public void setSize(Vector size) {
-        this.size = size;
-    }
+	public void setSize(Vector size) {
+		this.size = size;
+	}
 
-    public Long getAmount() {
-        return amount;
-    }
+	public Long getAmount() {
+		return amount;
+	}
 
-    public void setAmount(Long amount) {
-        this.amount = amount;
-    }
+	public void setAmount(Long amount) {
+		this.amount = amount;
+	}
 
-    public ArrayList<Image> getListImg() {
-        return listImg;
-    }
+	public ArrayList<Image> getListImg() {
+		return listImg;
+	}
 
-    public void setListImg(ArrayList<Image> listImg) {
-        this.listImg = listImg;
-    }
-    
-    public Long getArea() {
-        return size.getX()*size.getY();
-    }
+	public void setListImg(ArrayList<Image> listImg) {
+		this.listImg = listImg;
+	}
+
+	public Long getArea() {
+		return size.getX() * size.getY();
+	}
+
+	public void setPattern(Pattern pattern) {
+		this.amount = pattern.getAmount();
+		this.listImg = pattern.getListImg();
+		this.size = pattern.getSize();
+	}
 
     @Override
     public String toString() {
@@ -74,11 +80,27 @@ public class Pattern {
                 + "listImg : \n";
 
         for (Image img : listImg) {
-            listImgString.append(escToString(img));
-            listImgString.append('\n');
+            if(img != null && img.getAmount() != 0) {
+                listImgString.append(escToString(img));
+                listImgString.append('\n');
+            }
         }
         patternString = patternString + listImgString.toString();
 
         return patternString;
     }
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		Pattern cloned = (Pattern) super.clone();
+		cloned.setAmount(this.amount);
+		cloned.setSize(this.size);
+		cloned.listImg = new ArrayList<Image>();
+		for (Image image : this.listImg) {
+			cloned.listImg.add((Image) image.clone());
+		}
+		return cloned;
+	}
+
+
 }
