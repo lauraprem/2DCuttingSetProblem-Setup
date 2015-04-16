004e73b6970de876066451ce36ec2e572553c56e
Laura <premillieu.laura@gmail.com>
2015-04-16 10:40:34 +0200

    merge

--- 2DCuttingSetProblem-Setup/src/com/polytech4A/CSPS/core/tests/TestsLaura.java
+++ 2DCuttingSetProblem-Setup/src/com/polytech4A/CSPS/core/tests/TestsLaura.java
@@ -1,11 +1,47 @@
 package com.polytech4A.CSPS.core.tests;
 
+import java.util.ArrayList;
+
+import com.polytech4A.CSPS.core.method.verification.VerificationMethodImpl;
+import com.polytech4A.CSPS.core.model.Image;
+import com.polytech4A.CSPS.core.model.Pattern;
+import com.polytech4A.CSPS.core.model.Vector;
+import com.polytech4A.CSPS.core.util.PatternUtil;
+
 /**
  * @author Alexandre
  *         09/04/2015
  */
 public class TestsLaura {
 	public static void main(String[] args) {
+		Image image1 = new Image(1L, new Vector(12L, 12L), 352L);
+		image1.setAmount(1L);
+		Image image2 = new Image(2L, new Vector(2L, 2L), 100L);
+		image2.setAmount(2L);
+		Image image3 = new Image(3L, new Vector(1L, 1L), 25L);
+		image3.setAmount(0L);
+		ArrayList<Image> imageList1 = new ArrayList<Image>();
+		imageList1.add(image1);
+		imageList1.add(image2);
+		imageList1.add(image3);
+		Pattern pattern1 = new Pattern(new Vector(30L, 30L), imageList1);
 
+		VerificationMethodImpl verif = new VerificationMethodImpl();
+		// System.out.println(PatternUtil.addImage(pattern, 2L, verif));
+		// System.out.println(PatternUtil.supressImage(pattern, 2L, verif));
+
+		Image image4 = new Image(1L, new Vector(12L, 12L), 352L);
+		image1.setAmount(2L);
+		Image image5 = new Image(2L, new Vector(2L, 2L), 100L);
+		image2.setAmount(0L);
+		Image image6 = new Image(3L, new Vector(1L, 1L), 25L);
+		image3.setAmount(1L);
+		ArrayList<Image> imageList2 = new ArrayList<Image>();
+		imageList2.add(image4);
+		imageList2.add(image5);
+		imageList2.add(image6);
+		Pattern pattern2 = new Pattern(new Vector(30L, 30L), imageList2);
+
+		System.out.println(PatternUtil.exchangeImage(pattern1, pattern2, 2L, verif));
 	}
 }
