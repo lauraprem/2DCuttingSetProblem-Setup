9e476325f8f360d0e9e7782df73e65b3365c361e
agaldeano <alexandre.galdeano@etu.univ-lyon1.fr>
2015-05-09 13:22:56 +0200

    Chargement d'un fichier de data par une ligne de commande de la forme : path algo sizeOfPopulation amountOfGeneration

--- 2DCuttingStockProblem-Setup/src/com/polytech4A/CSPS/core/Program.java
+++ 2DCuttingStockProblem-Setup/src/com/polytech4A/CSPS/core/Program.java
@@ -1,12 +1,45 @@
 package com.polytech4A.CSPS.core;
 
+import com.polytech4A.CSPS.core.method.strategy.Genetic;
+import com.polytech4A.CSPS.core.method.strategy.StrategyMethod;
+import com.polytech4A.CSPS.core.method.verification.VerificationMethodImpl;
+import com.polytech4A.CSPS.core.model.Solution;
+import com.polytech4A.CSPS.core.resolution.Resolution;
+import com.polytech4A.CSPS.core.resolution.util.context.Context;
+import com.polytech4A.CSPS.core.resolution.util.context.ContextLoaderUtils;
+import com.polytech4A.CSPS.core.resolution.util.file.ToPNG;
+
+import java.io.IOException;
+
 /**
  * @author Alexandre
  *         13/03/2015
  */
 public class Program {
-	// TODO Surcharger tous les toString()
-	public static void main(String[] args) {
+    // TODO Surcharger tous les toString()
+    public static void main(String[] args) {
+        String path, algo;
+        if (args.length < 2) throw new RuntimeException();
+        path = args[0];
+        Context context;
+        StrategyMethod strategyMethod;
+        try {
+            context = ContextLoaderUtils.loadContext(path);
+            algo = args[1];
+            if ("genetic".equals(algo)) {
+                Integer
+                        populationSize = Integer.parseInt(args[2]),
+                        amountOfGeneration = Integer.parseInt(args[3]);
+                strategyMethod = new Genetic(context, new VerificationMethodImpl(), populationSize, amountOfGeneration);
+                strategyMethod.run();
 
-	}
+                Solution solution = strategyMethod.getBestSolution();
+                Resolution resolution = new Resolution(context);
+                resolution.setSolution(solution);
+                new ToPNG().save("genetic", resolution);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
